41d1c22069548f4296c41d7130f7c743218116ca
Diego <diegobattiston014@gmail.com>
2025-12-03 14:03:25 -0300

    adicionando mensagem caso a data do historico seja maior do que a data de hoje

--- src/app/components/history-modal/history.ts
+++ src/app/components/history-modal/history.ts
@@ -10,6 +10,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { HistoricoService } from '../../service/historico.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomSnackbar } from '../custom-snackbar/custom-snackbar';
 
 @Component({
   selector: 'app-history',
@@ -32,13 +34,14 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 export class History {
   constructor(
     private dialogRef: MatDialogRef<History>,
-    private historicoService: HistoricoService
+    private historicoService: HistoricoService,
+    private snackBar: MatSnackBar
   ) {}
 
   listHistorico: any[] = [];
   showList: boolean = false;
 
-  dataSelecionada = new FormControl<Date | null>(null, Validators.required);
+  dataSelecionada = new FormControl<Date | null>(null);
 
   close() {
     this.dialogRef.close();
@@ -50,10 +53,33 @@ export class History {
   }
 
   filter() {
-    console.log(this.dataSelecionada);
-    const data = this.dataSelecionada.value!.toISOString().split('T')[0];
+    const valor = this.dataSelecionada.value;
+    if (!valor) {
+      console.warn('Nenhuma data selecionada!');
+      return;
+    }
 
-    // quando a data filtrada nao tiver nenhum habito concluido, mostrar o toaste
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0);
+
+    const dataSelecionada = new Date(valor);
+    dataSelecionada.setHours(0, 0, 0, 0);
+
+    if (dataSelecionada > hoje) {
+      this.snackBar.openFromComponent(CustomSnackbar, {
+        data: {
+          message: 'Não é possível usar essa data!',
+          icon: 'error_outline',
+        },
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        panelClass: ['snackbar-error'],
+      });
+      return;
+    }
+
+    const data = this.dataSelecionada.value!.toISOString().split('T')[0];
 
     this.historicoService.getListHistoricoByDate(data).subscribe({
       next: (res) => {
