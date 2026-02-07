d16f76c419167449b1ad60b96b4dcf42d45c5996
Diego <diegobattiston014@gmail.com>
2026-02-07 18:48:30 -0300

    adicionando auth no login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HabtsPage } from './features/habts-page/habts-page';
 import { LoginPage } from './features/login-page/login-page';
 import { authGuard } from './service/auth-guard';
+import { loginGuard } from './service/login-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginPage },
+  { path: 'login', component: LoginPage, canActivate: [loginGuard] },
   { path: 'habits', component: HabtsPage, canActivate: [authGuard] },
 ];
