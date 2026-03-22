d6c68022e94e797bbc51954df6076f2b1a7e80e3
Diego <diegobattiston014@gmail.com>
2026-03-22 11:39:22 +0100

    criado estilos dos perfil e status e criado o formbuilder

--- src/app/features/create-user-page/create-user-page.ts
+++ src/app/features/create-user-page/create-user-page.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-user-page',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonToggleModule, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './create-user-page.html',
   styleUrl: './create-user-page.scss',
 })
-export class CreateUserPage {}
+export class CreateUserPage implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      nome: [''],
+      email: [''],
+      telefone: [''],
+      senha: ['ativo'],
+      status: ['ativo'],
+      perfil: ['user'],
+    });
+  }
+
+  showdata() {
+    console.log(this.form.value);
+  }
+}
